c83be55f19f828b28a773fed331cb8b8cb03afe4
Israel Posi Olatunbosun <israelbosun1@gmail.com>
2025-12-21 17:49:42 +0100

    added X

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { useAuth } from '@/context/AuthProvider'; // Ensure this path is correct
 import { useRouter, usePathname } from 'next/navigation';
+import { X } from 'lucide-react';
 
 export default function Header() {
   const { currentUser, logout } = useAuth();
